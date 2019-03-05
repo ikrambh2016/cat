c194775ade753455c3e5da075a3cec32bf2e463e
ikrambh2016 <48260181+ikrambh2016@users.noreply.github.com>
2019-03-05 22:28:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static(__dirname + '/dist/facemash'));
 
 app.get('/*', function(req,res) {
 
-res.sendFile(path.join(__dirname+'/dist/facemash/src/index.html'));
+res.sendFile(path.join(__dirname+'/dist/facemash/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
